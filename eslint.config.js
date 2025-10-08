bf8836d19b0fdeae3a94a9c68f48c9d1a32cbe29
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-10-08 23:15:17 +0900

    chore: react-refreshの設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
 import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
@@ -23,10 +24,13 @@ export default [
     plugins: {
       '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
+
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
       // React Hooks
       'react-hooks/rules-of-hooks': 'error',
