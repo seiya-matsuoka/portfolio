9e9a623d9c7a26e9a7e4e8184d7671d2b2954eea
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-06-23 22:28:12 +0900

    fix: ページ遷移時に先頭へスクロールするよう改修

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { ScrollToTop } from '../components/common/ScrollToTop';
 import { SkipLink } from '../components/common/SkipLink';
 
 export function AppLayout() {
   return (
     <div className="min-h-dvh bg-[color:var(--color-bg,theme(colors.slate.50))] text-[color:var(--color-fg,theme(colors.slate.800))]">
+      <ScrollToTop />
       <SkipLink />
       <Header />
 
